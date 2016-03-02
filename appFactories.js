cfd4c66e08997dd92b45cf0bd2621880d1504ab1
Juan <jguardado85@gmail.com>
2016-03-01 21:17:12 -0800

    cleaning up files

--- appFactories.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('myAppFactories', []);
-
-
-  app.factory('historFactory', [], function(){
-    chartHistory = function(){
-      //some how creates a chart for the history
-    }
-  })
